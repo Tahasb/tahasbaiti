c1fa376ad1f89e466fe2bd7ca5b72743ac8023cc
Tahasb <sbaiti.taha@gmail.com>
2022-02-19 16:02:08 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 const toggleButton = document.querySelector(".mobile-nav-toggle");
 const navBar = document.querySelector(".nav-bar");
 const fade = document.querySelectorAll(".fade");
+const projectImg = document.querySelector(".img-wrap");
+const options = {
+  threshold: 0.3,
+};
 
 fade.forEach((e) => {
   e.addEventListener("click", eventHandler);
@@ -17,3 +21,15 @@ function eventHandler() {
     toggleButton.setAttribute("aria-expanded", false);
   }
 }
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("appear");
+      observer.unobserve(entry.target);
+    } else {
+      return;
+      //entry.target.classList.remove("appear");
+    }
+  });
+}, options);
+observer.observe(projectImg);
